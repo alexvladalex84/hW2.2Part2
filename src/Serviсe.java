6e34a71fe9bea9122dae6c86706887bb781de79a
Alexandrov <alex.44747.alex@gmail.com>
2023-05-16 14:01:50 +0300

    домашка 2.2 часть2

---
+++ src/Serviсe.java
@@ -0,0 +1,14 @@
+public interface Serviсe {
+
+
+    public static void updateTyre() {
+        System.out.println(" Меняем покрышку ");
+    }
+    static void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+
+    static void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
+}
